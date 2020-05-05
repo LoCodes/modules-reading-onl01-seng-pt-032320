f0dfbe24023e26c629168806570609016a4a9ded
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-05 02:55:28 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,20 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+#
+# Kid
+#   includes the Dance module (FAILED - 8)
+#   has a name (FAILED - 9)
+#   extends the MetaDancing module (FAILED - 10)
+
+
+class Kid
+  extend MetaDancing
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
