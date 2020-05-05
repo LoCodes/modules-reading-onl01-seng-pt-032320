fa27d1ce764e9c2ed31aedd6b6c87ac41485ff59
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-05 03:01:29 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -8,11 +8,11 @@ require_relative './class_methods_module.rb'
 #   extends the MetaDancing module (FAILED - 10)
 
 class Kid
-  # include Dance
-  # extend MetaDancing
-
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  include Dance
+  extend MetaDancing
+  # 
+  # extend FancyDance::ClassMethods
+  # include FancyDance::InstanceMethods
 
 
   attr_accessor :name
